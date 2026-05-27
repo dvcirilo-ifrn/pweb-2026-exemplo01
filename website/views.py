49c20f356290efdb95cbe6bcfb1e5f7fe3dabd75
Diego Cirilo <dvcirilo@gmail.com>
2026-05-27 10:21:51 -0300

    aula 27/05

--- website/views.py
+++ website/views.py
@@ -4,7 +4,27 @@ def index(request):
     return render(request, 'website/index.html')
 
 def pratos(request):
-    return render(request, 'website/pratos.html')
+    pratos = [
+        {"nome": "Bauru",
+         "ingredientes": "Pão, ovo, queijo, presunto, carne e salada.",
+         "preco": "R$ 10,00",
+         "tempo": "10min",
+         "imagem": "website/assets/img/portfolio/cabin.png",
+        },
+        {"nome": "Lasanha",
+         "ingredientes": "Macarrão, molho, carne, presunto, queijo.",
+         "preco": "R$ 70,00",
+         "tempo": "1h30min",
+         "imagem": "website/assets/img/portfolio/controller.png",
+        },
+        {"nome": "Tapioca",
+         "ingredientes": "Goma",
+         "preco": "R$ 2,50",
+         "tempo": "10min",
+        },
+    ]
+    context = {"pratos": pratos}
+    return render(request, 'website/pratos.html', context)
 
 def sobre(request):
     return render(request, 'website/sobre.html')
