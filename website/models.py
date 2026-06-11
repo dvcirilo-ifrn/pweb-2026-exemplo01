253de01caccdd077a72b3f6d5a4472df4e800be1
Diego Cirilo <dvcirilo@gmail.com>
2026-06-11 12:00:02 -0300

    models 11-06

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Prato(models.Model):
+    nome = models.CharField(max_length=100)
+    ingredientes = models.CharField(max_length=200)
+    preco = models.DecimalField(decimal_places=2, max_digits=5)
+    tempo = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
