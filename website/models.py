519d8b75a7b7a56caf7614a26e9473d21237385d
Diego Cirilo <dvcirilo@gmail.com>
2026-06-17 10:21:17 -0300

    adicionando imagem 17/06

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ class Prato(models.Model):
     ingredientes = models.CharField(max_length=200)
     preco = models.DecimalField(decimal_places=2, max_digits=5)
     tempo = models.IntegerField()
+    imagem = models.ImageField(upload_to="pratos/", blank=True)
 
     def __str__(self):
         return self.nome
